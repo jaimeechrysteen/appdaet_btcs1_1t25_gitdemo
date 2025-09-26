9a5e0cc1238a52f0285b9c98fa4d5edf8219e106
Jaimee Chrysteen P. Palpal-latoc <jaimeechrysteen.palpallatoc@benilde.edu.ph>
2025-09-26 23:06:52 +0800

    Jaimee's revision for Lab Activity 3

---
+++ jaimeepalpallatoc.py
@@ -0,0 +1,9 @@
+# Jaimee Palpal-latoc
+
+def cool_banner(message):
+    border = "*" * (len(message) + 8)
+    print(border)
+    print(f"*** {message} ***")
+    print(border)
+
+cool_banner("This is a revision for Lab Activity 3 by Jaimee Palpal-latoc")
